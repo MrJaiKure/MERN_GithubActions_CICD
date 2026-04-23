33c09b61ef553f54f1fbf802baf2b4c61abd5026
MrJaiKure <kurejaideep2003@gmail.com>
2026-04-23 20:07:21 +0530

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('🚀 DevOps CI/CD Pipeline is Working!');
+  res.send('🚀 DevOps CI/CD Pipeline is Working! really');
 });
 
 app.get('/health', (req, res) => {
